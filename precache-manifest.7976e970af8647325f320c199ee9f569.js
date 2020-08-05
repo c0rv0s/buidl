e62b8c4c2cfeecad2b19d942ce58565163cc9f0d
Nathanael Mueller <nathanaelmueller@Nates-Mac.local>
2020-08-05 14:23:03 -0700

    Updates

---
+++ precache-manifest.7976e970af8647325f320c199ee9f569.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c30b874dc30ca0d3d4543c0b6d798f",
+    "url": "/buidl/index.html"
+  },
+  {
+    "revision": "add0e9d12f9af26f8ba2",
+    "url": "/buidl/static/css/main.f3f73225.chunk.css"
+  },
+  {
+    "revision": "094dc2bdccf2deeb7427",
+    "url": "/buidl/static/js/2.e2f82d03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/buidl/static/js/2.e2f82d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add0e9d12f9af26f8ba2",
+    "url": "/buidl/static/js/main.157c0f7e.chunk.js"
+  },
+  {
+    "revision": "14d8e82c5384fd8ff74b",
+    "url": "/buidl/static/js/runtime-main.39a465f6.js"
+  }
+]);
