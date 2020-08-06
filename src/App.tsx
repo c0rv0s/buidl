d05ea455c4c8c273c8f1805f3eb054a1f4afd43a
Nathanael Mueller <nathanaelmueller@Nates-Mac.local>
2020-08-05 21:40:27 -0700

    title, messages, word logic

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,14 @@ function App() {
     for (let i in words) {
       let word = words[i].toLowerCase()
       let numL = (word.match(/l/g) || []).length 
-      let newWord = word.replace(/l/g, "") + "l".repeat(numL)
+      let newWord: string = ""
+      if (word.substring(word.length-2) === "er") {
+        newWord = word.replace(/l/g, "")
+        newWord = newWord.substring(0, word.length-3) + "l".repeat(numL) + "er"
+      }
+      else {
+        newWord = word.replace(/l/g, "") + "l".repeat(numL)
+      }
       newWords[i] = newWord
     }
     setText(text)
@@ -20,9 +27,11 @@ function App() {
 
   return (
     <div className="App">
-      <i>Buidl it</i>
+      <i>If you can dream it, you can meme it</i>
       <br/>
-      <textarea placeholder="Start typing..." onChange={(x) => translate(x.target.value)} value={text}/>
+      <textarea placeholder="Try typing something with the letter L..." onChange={(x) => translate(x.target.value)} value={text}/>
+      <br/><br/>
+      {translated.length > 0 && <i>Behold...</i>}
       <p>{translated}</p>
       <footer className="footer">
         Made by <a href="https://onenathan.codes">Nathan</a>
